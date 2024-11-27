bd9eecbe134e4dd9c22d26272e035e115aa6307c
Gabriel Feltes dos Santos <gabrielfeltes.gg@hotmail.com>
2024-11-27 20:42:24 -0300

    good practice

--- backend/utils.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-# Configuração de logs
-logging.basicConfig(filename="backend.log", level=logging.INFO, format="%(asctime)s - %(message)s")
-
-def log_event(message):
-    """Registra eventos importantes no log."""
-    logging.info(message)
-    print(message)  # Também imprime no console para depuração
